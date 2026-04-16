898c412aa1742594ac71c72727d0ca66dfe578fc
Romain <115707312+webromain@users.noreply.github.com>
2026-04-16 23:13:38 +0200

    security: require SECRET_KEY; fix: collectstatic build;

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-s@9bjikevgmlkb0%pvhuhpxb04owg00&i@2&m*=%n7r#$2+k&8')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+if not SECRET_KEY:
+    raise RuntimeError("SECRET_KEY environment variable is not set")
 
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
